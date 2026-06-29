2e6d6b994c51458b76a61dbfbdd9e98dc21b1f20
Felix <xi.lifeng@qq.com>
2026-06-29 16:50:00 +0800

    fix: use less-bent posture in xyz test too

--- block_grasp/test_move_to_xyz.py
+++ block_grasp/test_move_to_xyz.py
@@ -12,7 +12,7 @@ robot = HeadArmRobot(urdf_path=urdf, dev="/dev/ttyUSB0", baudrate=1_000_000)
 hand = DexHand(hand_type="right", can_iface="can0", baudrate=1_000_000)
 
 # Safe posture
-q_init = np.radians([0, 0, 0, -60, 60, 0, 0, 0])
+q_init = np.radians([0, 0, 0, -30, 30, 0, 0, 0])
 robot.set_positions(q_init)
 robot.wait_until_reached(q_init, active_joint_indices=range(8))
 time.sleep(0.3)
